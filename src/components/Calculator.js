ba4e1bf89d92cf67f3f9b29de446604a08b8e1bc
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-05-30 23:43:50 +0530

    Update calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,28 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = ({ type, label }) => (
+  <button type="button" className={`button ${type}`}>
+    {label}
+  </button>
+);
+
+Button.propTypes = {
+  type: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+};
 
 const Calculator = () => (
   <div className="calculator">
     <div className="display">0</div>
     <div className="keypad">
-      <button type="button" className="button">AC</button>
-      <button type="button" className="button">+/-</button>
-      <button type="button" className="button">%</button>
-      <button type="button" className="button orange">÷</button>
-      <button type="button" className="button">7</button>
-      <button type="button" className="button">8</button>
-      <button type="button" className="button">9</button>
-      <button type="button" className="button orange">x</button>
-      <button type="button" className="button">4</button>
-      <button type="button" className="button">5</button>
-      <button type="button" className="button">6</button>
-      <button type="button" className="button orange">-</button>
-      <button type="button" className="button">1</button>
-      <button type="button" className="button">2</button>
-      <button type="button" className="button">3</button>
-      <button type="button" className="button orange">+</button>
-      <button type="button" className="button large">0</button>
-      <button type="button" className="button">.</button>
-      <button type="button" className="button orange">=</button>
+      <Button type="normal" label="AC" />
+      <Button type="normal" label="+/-" />
+      <Button type="normal" label="%" />
+      <Button type="orange" label="÷" />
+      <Button type="normal" label="7" />
+      <Button type="normal" label="8" />
+      <Button type="normal" label="9" />
+      <Button type="orange" label="x" />
+      <Button type="normal" label="4" />
+      <Button type="normal" label="5" />
+      <Button type="normal" label="6" />
+      <Button type="orange" label="-" />
+      <Button type="normal" label="1" />
+      <Button type="normal" label="2" />
+      <Button type="normal" label="3" />
+      <Button type="orange" label="+" />
+      <Button type="large" label="0" />
+      <Button type="normal" label="." />
+      <Button type="orange" label="=" />
     </div>
   </div>
 );
