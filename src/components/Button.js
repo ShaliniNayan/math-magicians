4c6e04b30a1122468b4bac6c85694a40f9e112ed
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-05-31 21:19:04 +0530

    Add new dir logic and 2 file calculate & operate, Update Calculate & Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
-const Button = ({ type, label }) => (
-  <button type="button" className={`button ${type}`}>
+const Button = ({ type, label, onClick }) => (
+  <button type="button" className={`button ${type}`} onClick={onClick}>
     {label}
   </button>
 );
@@ -9,6 +9,7 @@ const Button = ({ type, label }) => (
 Button.propTypes = {
   type: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default Button;
